25295a7ced88f97cb23cf821f813fe8f09016f59
lspangenberg <laurens.am.spangenberg@gmail.com>
2022-05-30 17:10:03 +0200

    create new user and page upon signup

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const User = require("../models/User");
 const router = Router();
 const bcrypt = require("bcrypt");
+const Page = require("../models/Page");
 
 router.post("/login", async(req, res) => {
   try {
@@ -30,10 +31,32 @@ router.post("/signup", async(req, res) => {
 
     if (!email || !password || !username || !name) return res.status(400).send("Request is missing an email, password, username, or name");
 
+    const emailExists = await User.exists({email: email});
+    if (emailExists) return res.status(400).send(`user with email: ${email} already exists`);
+
+    const usernameExists = await User.exists({username: username});
+    if (usernameExists) return res.status(400).send(`user with username: ${username} already exists`);
+
     //TODO: create new User document and Page document
     //send back token and Page document
 
-    res.send("signup succesful");
+    const newUser = await User.create({
+      email, password, username, name
+    })
+
+    const newPage = await Page.create({
+      name: newUser.name,
+      username: newUser.username,
+      userId: firstUser._id,
+      colors: {},
+      permaLinks: [],
+      oneLiner: "",
+      bio: "",
+      sectionOrdering: [],
+      sections: []
+    });
+
+    res.send(newPage);
 
   } catch (error) {
     console.log(error);
