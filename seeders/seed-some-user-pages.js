d345b4865894013550eb962bc546ea46d3db50b3
lspangenberg <laurens.am.spangenberg@gmail.com>
2022-05-30 14:13:00 +0200

    add seeder

---
+++ seeders/seed-some-user-pages.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const authKey = require("../auth");
+const Page = require("../models/Page");
+const User = require("../models/User");
+
+mongoose.connect(authKey);
+
+// This is a very ugly way of doing
+// it and just shown as a quick and 
+// dirty example
+
+const seedSomeUsersAndPages = async () => {
+  try {
+    const firstUser = await User.create({
+      email: "laurens@laurens.com",
+      password: "laurenspassword",
+      username: "laurensuser",
+      name: "laurens",
+    });
+
+    const secondUser = await User.create({
+      email: "david@david.com",
+      password: "davidpassword",
+      username: "daviduser",
+      name: "david",
+    });
+
+    const firstPage = await Page.create({
+      name: laurens,
+      username: laurensuser,
+      userId: firstUser._id,
+      colors: {},
+      permaLinks: [],
+      oneLiner: "",
+      bio: "",
+      sectionOrdering: [],
+      sections: []
+    })
+
+    const secondPage = await Page.create({
+      name: david,
+      username: daviduser,
+      userId: secondUser._id,
+      colors: {},
+      permaLinks: [],
+      oneLiner: "",
+      bio: "",
+      sectionOrdering: [],
+      sections: []
+    })
+
+    console.log(firstPage, secondPage);
+    process.exit();
+  } catch (error) {
+    process.exit();
+  }
+};
+
+seedSomeUsersAndPages();
